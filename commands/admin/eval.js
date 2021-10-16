189842e0717951612e97e8ba57fd1e65a098f069
quentin72000 <quentin72000@yahoo.com>
2021-10-16 13:55:57 +0200

    Added eval message command Moved check message command to folder admin

---
+++ commands/admin/eval.js
@@ -0,0 +1,39 @@
+const { Message, Client, Permissions:{FLAGS: perms} } = require("discord.js");
+module.exports = {
+    name: "eval",
+    aliases: ['exec', "execute"],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+        if(!client.config.whitelistUsers.includes(message.author.id))return message.reply("Error: You can't do that.");
+        if(message.author.id !==  "611938209366016000") return;
+        let content = message.content.split(" ").slice(1).join(" ");
+        if (content.includes("env.TOKEN")) content.replace("env.TOKEN", "NOP");
+        if (content.includes("config.token")) content.replace("config.token", "NOP")
+        const result = new Promise((resolve, reject) => resolve(eval(content)));
+        
+        return result.then((output) => {
+            if(typeof output !== "string"){
+                output = require("util").inspect(output, { depth: 0 });
+            }
+            if(output.includes(client.token)){
+                output = output.replace(message.client.token, "T0K3N");
+            }
+            message.channel.send(output, {
+                code: "js"
+            });  
+        }).catch((err) => {
+            err = err.toString();
+            if(err.includes(message.client.token)){
+                err = err.replace(message.client.token, "T0K3N");
+            }
+            message.channel.send(err, {
+                code: "js"
+            });
+        });
+    },
+};
