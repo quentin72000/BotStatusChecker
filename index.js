450fad93d9b76b0117b9598aace1a2ada595e31a
quentin72000 <quentin72000@yahoo.com>
2022-02-03 18:24:22 +0100

    Added comment Edit the code to create the DB to make it async

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const client = new Client({
 const sqlite = require("sqlite3")
 let db = new sqlite.Database('./db.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE)
 
-db.all("SELECT name FROM sqlite_master WHERE type='table'", (err, result) => {
+db.all("SELECT name FROM sqlite_master WHERE type='table'", (err, result) => { // check if db is empty or incomplete
     if(err) return console.error(err)
-    if(!result || !(result.some(e => e.name = "users") && result.some(e => e.name = "guilds"))){
+    if(!result || !(result.some(e => e.name = "users") && result.some(e => e.name = "guilds"))){ // (re)create it if empty or incomplete
         console.log("DB is empty, missing a table or is corrupted, (re)creating...");
         createDBTable()
     }
@@ -37,9 +37,22 @@ client.login(process.env.TOKEN);
 
 async function createDBTable () {
     db.serialize(async() =>{
-        await db.run(db_request.create.users) // create users table
-        await db.run(db_request.create.guilds) // create guilds table
-        console.log("Database successfully (re)created")
+        await db.run(db_request.create.users, (err) => { // create users table
+            if(err){
+                console.error("An error occurred while creating users table")
+                console.error(err);
+                prosses.exit(1)
+            }
+        })
+        await db.run(db_request.create.guilds, (err) => { // create guilds table
+            if(err){
+                console.error("An error occurred while creating users table")
+                console.error(err);
+                return prosses.exit(1)
+            }
+        else console.log("Database successfully (re)created")
+
+        })
     })
     
 }
